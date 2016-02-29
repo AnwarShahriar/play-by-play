cddc56a336046a15b53b385c0e7257306c80b26c
anwarshahriar <anwarshahriar@gmail.com>
2016-03-01 01:28:46 +0600

    Adds mongodb for data storage

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-var cats = require('./cats.js')(app);
+var cats = require('./cat_routes.js')(app);
 
 var server = app.listen(3000, function () {
 	console.log('listening to http://127.0.0.1:3000/');
