d4c0c0dac9db673748504854c43de2aff460c5e2
shishir <anwarshahriar@gmail.com>
2016-03-01 16:18:26 +0600

    Adds dog route, model and refactors code

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cats');
+mongoose.connect('mongodb://localhost/dogs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-var cats = require('./cat_routes.js')(app);
+var cats = require('./routes/dog.js')(app);
 
 var server = app.listen(3000, function () {
 	console.log('listening to http://127.0.0.1:3000/');
